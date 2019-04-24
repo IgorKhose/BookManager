547ebadd94879882dcfedbfd0abed844533dedfe
IKOS <igorkhose@gmail.com>
2019-04-24 10:06:03 +0300

    Initial commit

---
+++ src/main/java/com/gotham/bookmanager/service/BookService.java
@@ -0,0 +1,13 @@
+package com.gotham.bookmanager.service;
+
+import com.gotham.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public void addBook(Book book);
+    public void updateBook(Book book);
+    public void removeBook(int id);
+    public Book getBookById(int id);
+    public List<Book> listBooks();
+}
